29687d3ef306c4b3732eefe31209ad97965a63d8
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-05 18:02:16 +0200

    second commit

---
+++ read.js
@@ -0,0 +1,24 @@
+//require filesystem
+const fs = require('fs');
+
+//read async files from the folder 
+fs.readdir('.', 'utf8', (error, arrFiles)=>{
+    if(error){
+        console.error(error);
+        return;
+    }
+    for(let f of arrFiles){
+        if(f == 'config.json'){
+            fs.readFile(f, 'utf8', (error,data)=>{
+                if(error){
+                    console.error("Error reading file");
+                    return;
+                } else{
+                    console.log(data);
+                }
+            })
+        }
+        console.log(f);
+    }
+})
+
