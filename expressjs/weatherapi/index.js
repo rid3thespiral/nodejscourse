df32068f3c717434e0e5ec056ff6043ca12a04b6
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-08 13:23:25 +0200

    express js weather api

---
+++ expressjs/weatherapi/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//return an instance of http.Server
+app.listen(3000);
+app.get('/',(req,res)=>{
+    res.send({name:'Hidran',lastname:'Ole'});
+});
+
+//placeholders parameters
+app.get('/getWeather/:zip([0-9]+)',(req,res)=>{
+    res.send(req.params.zip);
+});
+
+//placeholders city
+app.get('/getWeather/:city([a-zA-Z]+)',(req,res)=>{
+    res.send(req.params.city);
+});
