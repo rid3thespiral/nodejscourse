75e8f11a8039d31c094727510a41c51eb551a3fe
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-08 13:35:32 +0200

    static files through app use

--- expressjs/weatherapi/index.js
+++ expressjs/weatherapi/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 //return an instance of http.Server
 app.listen(3000);
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.send({name:'Hidran',lastname:'Ole'});
 });
@@ -16,3 +17,4 @@ app.get('/getWeather/:zip([0-9]+)',(req,res)=>{
 app.get('/getWeather/:city([a-zA-Z]+)',(req,res)=>{
     res.send(req.params.city);
 });
+
